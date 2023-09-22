b2d965eddbbae9127dcbc5b229905e7f8f68a85b
mihail_sheludyakov <bvg2003@bk.ru>
2023-09-23 01:00:33 +0300

    Order Aggregate Root fields

--- common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/OrderId.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/OrderId.java
@@ -3,7 +3,6 @@ package com.food.ordering.system.domain.valueobject;
 import java.util.*;
 
 public class OrderId extends BaseId<UUID> {
-
     protected OrderId(UUID value) {
         super(value);
     }
