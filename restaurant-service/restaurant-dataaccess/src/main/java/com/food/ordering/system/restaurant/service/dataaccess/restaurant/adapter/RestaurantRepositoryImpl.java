eca79a10422a7656b0597fec9a3f0f9fdc290b82
Shell2016 <bvg2003@bk.ru>
2023-11-20 20:53:06 +0300

    restaurant-service: container

--- restaurant-service/restaurant-dataaccess/src/main/java/com/food/ordering/system/restaurant/service/dataaccess/restaurant/adapter/RestaurantRepositoryImpl.java
+++ restaurant-service/restaurant-dataaccess/src/main/java/com/food/ordering/system/restaurant/service/dataaccess/restaurant/adapter/RestaurantRepositoryImpl.java
@@ -1,8 +1,8 @@
-package com.food.ordering.system.payment.service.dataaccess.restaurant.adapter;
+package com.food.ordering.system.restaurant.service.dataaccess.restaurant.adapter;
 
 import com.food.ordering.system.dataaccess.restaurant.entity.RestaurantEntity;
 import com.food.ordering.system.dataaccess.restaurant.repository.RestaurantJpaRepository;
-import com.food.ordering.system.payment.service.dataaccess.restaurant.mapper.RestaurantDataAccessMapper;
+import com.food.ordering.system.restaurant.service.dataaccess.restaurant.mapper.RestaurantDataAccessMapper;
 import com.food.ordering.system.restauranst.service.domain.ports.output.repository.RestaurantRepository;
 import com.food.ordering.system.restaurant.service.domain.entity.Restaurant;
 import lombok.AccessLevel;
