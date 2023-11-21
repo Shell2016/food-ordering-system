69d5e6b9a3ad1e53ff6d28f3a01a7adad73205f6
Shell2016 <bvg2003@bk.ru>
2023-11-21 13:50:54 +0300

    order-service: saga * SagaStep * OrderPaymentSaga

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
@@ -1,7 +1,9 @@
 package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
 import com.food.ordering.system.order.service.domain.port.input.message.listener.payment.PaymentResponseMessageListener;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -9,14 +11,19 @@ import org.springframework.validation.annotation.Validated;
 @Slf4j
 @Validated
 @Service
+@RequiredArgsConstructor
 public class PaymentResponseMessageListenerImpl implements PaymentResponseMessageListener {
+
+    private final OrderPaymentSaga orderPaymentSaga;
+
     @Override
     public void paymentCompleted(PaymentResponse paymentResponse) {
-
+        OrderPaidEvent orderPaidEvent = orderPaymentSaga.process(paymentResponse);
+        orderPaidEvent.fire();
     }
 
     @Override
     public void paymentCancelled(PaymentResponse paymentResponse) {
-
+        orderPaymentSaga.rollback(paymentResponse);
     }
 }
